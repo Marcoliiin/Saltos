3c26010f761196509057f2a341411e3224248da6
Gabriel Marcolin de Carvalho <146651956+Marcoliiin@users.noreply.github.com>
2023-10-05 21:31:07 -0300

    Saltosv2
    Quase terminando esse exercício, falta eu conseguir printar todos os atletas, to conseguindo printar só um. Imagino que eu tenha que usar algum laço de repetição ou usar o for each msm. Da próxima vez que eu tiver tempo eu vou fazer alguns testes

--- Saltos.js
+++ Saltos.js
@@ -1,50 +1,62 @@
 var atletas = []
 
 
-class atleta{
-   constructor(nome,s1,s2,s3,s4,s5){
-   this.nome = nome
-   this.s1 = s1;
-   this.s2 = s2;
-   this.s3 = s3;
-   this.s4 = s4;
-   this.s5 = s5;  
- }
+class atleta {
+   constructor(nome, s1, s2, s3, s4, s5) {
+      this.nome = nome
+      this.s1 = s1;
+      this.s2 = s2;
+      this.s3 = s3;
+      this.s4 = s4;
+      this.s5 = s5;
 
-   st (){
-   return ("Saltos do atleta " + this.nome + ": " + this.s1 + " - " + this.s2 + " - " + this.s3 + " - " + this.s4 + " - " + this.s5);   
    }
-   
-   sm (){
-   return ("A média dos seus do atleta " + this.nome + " é: ") + parseFloat((this.s1 + this.s2 + this.s3 + this.s4 + this.s5)/5)    
-   }
-}      
+   nome = ""
+   s1 = 0;
+   s2 = 0;
+   s3 = 0;
+   s4 = 0;
+   s5 = 0;
 
-   function criarAtleta(nome,s1,s2,s3,s4,s5){
-   const newatleta = new atleta (nome,s1,s2,s3,s4,s5)
-   atletas[0].push(newatleta)
-
-   window.document.getElementById("textinput1").value=("");
-   window.document.getElementById("textinput2").value=("");
-   window.document.getElementById("textinput3").value=("");
-   window.document.getElementById("textinput4").value=("");
-   window.document.getElementById("textinput5").value=("");
-   window.document.getElementById("textinput6").value=("");
+   st() {
+      return("Saltos do atleta " + this.nome + ": " + this.s1 + " - " + this.s2 + " - " + this.s3 + " - " + this.s4 + " - " + this.s5);
    }
 
-   function adicionarAtleta(){
-      
-   let nome = document.getElementById("textinput1").value;
-   let s1 = document.getElementById("textinput2").value;
-   let s2 = document.getElementById("textinput3").value;
-   let s3 = document.getElementById("textinput4").value;
-   let s4 = document.getElementById("textinput5").value;
-   let s5 = document.getElementById("textinput6").value;
-
-   criarAtleta(nome, s1,s2,s3,s4,s5)
+   sm() {
+      return("A média dos seus do atleta " + this.nome + " é: ") + (parseFloat(this.s1) + parseFloat(this.s2) + parseFloat(this.s3) + parseFloat(this.s4) + parseFloat(this.s5))/5
+   }
 }
 
-function mostrarAtletas(){
-     let x = 0
-      console.log(atletas[x])
-      }
+function criarAtleta(nome, s1, s2, s3, s4, s5) {
+   const newatleta = new atleta(nome, s1, s2, s3, s4, s5)
+   atletas.push(newatleta)
+
+   window.document.getElementById("textinput1").value = ("")
+   window.document.getElementById("textinput2").value = ("")
+   window.document.getElementById("textinput3").value = ("")
+   window.document.getElementById("textinput4").value = ("")
+   window.document.getElementById("textinput5").value = ("")
+   window.document.getElementById("textinput6").value = ("")
+}
+
+function adicionarAtleta() {
+
+   let nome = document.getElementById("textinput1").value
+   let s1 = document.getElementById("textinput2").value
+   let s2 = document.getElementById("textinput3").value
+   let s3 = document.getElementById("textinput4").value
+   let s4 = document.getElementById("textinput5").value
+   let s5 = document.getElementById("textinput6").value
+
+   criarAtleta(nome, s1, s2, s3, s4, s5)
+}
+
+function mostrarAtletas() {
+   let show1 = document.getElementById("result1"); 
+   let show2 = document.getElementById("result2");
+   
+   atletas.forEach(element => {
+      show1.innerHTML = element.st(); 
+      show2.innerHTML= element.sm();
+   });
+}
